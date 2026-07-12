2ca7509cc09ee76582fa5ca7206300a483ddfee7
Purbayan <purbayan.00@gmail.com>
2026-07-12 10:49:06 +0530

    Replace rofi with wofi for script selection

--- config_dotfiles/config/custom_scripts/executeScripts.sh
+++ config_dotfiles/config/custom_scripts/executeScripts.sh
@@ -5,7 +5,7 @@ scriptsDir="$HOME/.local/share/config_dotfiles/config/custom_scripts/executables
 selected_script=$(
   find "$scriptsDir" -maxdepth 1 -type f -executable ! -name '.*' -printf '%f\n' |
   sort |
-  rofi -dmenu -i -matching fuzzy -p "scripts"
+  wofi --dmenu -i --prompt "scripts" --width 500
 )
 
 [[ -z "${selected_script:-}" ]] && exit 0
